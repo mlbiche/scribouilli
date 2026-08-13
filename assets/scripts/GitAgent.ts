30be28c0caa6151e49117ac245b7c8311a6ff283
mlbiche <tmlb@pm.me>
2026-08-13 17:41:16 +0200

    Remove some dead code

--- assets/scripts/GitAgent.ts
+++ assets/scripts/GitAgent.ts
@@ -346,8 +346,6 @@ export default class GitAgent {
    *
    *
    * https://isomorphic-git.org/docs/en/setConfig
-   *
-   * Alors, on doit passer le repoName
    */
   async setAuthor(
     login: string,
